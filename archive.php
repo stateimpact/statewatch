aab5a81ee211d4a423b377d1723e74e21e0f40d0
Chris Amico <eyeseast@gmail.com>
2011-05-19 12:08:11 -0700

    Added author name, title and description to author archives, with some template logic. Still needs that Wes/Danny touch.

---
+++ archive.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ */
+?>
+
+<?php get_header(); ?>
+
+<div id="content" class="grid_8" role="main">
+
+<?php
+	/* Queue the first post, that way we know
+	 * what date we're dealing with (if that is the case).
+	 *
+	 * We reset this later so we can run the loop
+	 * properly with a call to rewind_posts().
+	 */
+	if ( have_posts() )
+		the_post();
+?>
+
+<?php if ( !is_author() ): ?>
+<nav class="archive-dropdown">
+<select name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'> <option value=""><?php echo esc_attr(__('Select Month')); ?></option> <?php wp_get_archives(apply_filters('widget_archives_dropdown_args', array('type' => 'monthly', 'format' => 'option', 'show_post_count' => $c))); ?> </select>
+</nav>
+<?php endif; ?>
+
+<h3 class="page-title">
+    <?php if ( is_month() ) : ?>
+        <?php printf( 'Monthly Archives: <span>%s</span>', get_the_date('F Y') ); ?>
+    <?php elseif ( is_year() ) : ?>
+        <?php printf( 'Yearly Archives: <span>%s</span>', get_the_date('Y') ); ?>
+    <?php elseif ( is_author() ) : ?>
+        <?php printf( 'Author Archives: <span class="author">%s</span>', get_the_author() ); ?>
+    <?php else : ?>
+        Blog Archives
+    <?php endif; ?>
+</h3>
+	<?php if ( is_author() ): ?>
+	<h4><?php the_author_meta( 'sw_title' ); ?></h4>
+	<div class="author-description">
+	    <?php the_author_meta( 'description' ); ?>
+	</div>
+	<?php endif; ?>
+<nav>		
+<ul class="list-pagination">
+    <li class="older-posts"><?php next_posts_link( 'Older posts' ); ?></li>
+    <li class="newer-posts"><?php previous_posts_link( 'Newer posts' ); ?></li>
+</ul>
+</nav>
+<!--/.pagination-->
+
+<?php
+	/* Since we called the_post() above, we need to
+	 * rewind the loop back to the beginning that way
+	 * we can run the loop properly, in full.
+	 */
+	 rewind_posts();
+
+	/* Run the loop for the archives page to output the posts.
+	 * If you want to overload this in a child theme then include a file
+	 * called loop-archives.php and that will be used instead.
+	 */
+	 get_template_part( 'loop', 'archive' );
+?>
+			
+</div><!--/ #content .grid_8-->
+
+<aside id="sidebar" class="grid_4">
+<?php get_sidebar(); ?>
+</aside>
+<!-- /.grid_4 -->
+
+<?php get_footer(); ?>
