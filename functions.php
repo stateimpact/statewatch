eb868880b1ea0e84425354343fb2b3c7f3dc6650
Chris Amico <eyeseast@gmail.com>
2011-05-23 11:13:58 -0700

    Station post types added. Still need to customize.

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
 // includes
 require_once( 'includes/users.php' );
-
+require_once( 'includes/stations.php' );
 ?>
---
+++ includes/stations.php
@@ -0,0 +1,27 @@
+<?php
+add_action( 'init', 'create_station_post_types' );
+
+function create_station_post_types() {
+    register_post_type( 'partner_station', 
+        array(
+            'labels' => array(
+                'name' => 'Partner Stations',
+                'singular_name' => 'Partner Station',
+                'add_new' => 'Add New',
+                'add_new_item' => 'Add New Station',
+                'edit' => 'Edit',
+                'edit_item' => 'Edit Station',
+                'view' => 'View',
+                'view_item' => 'View Station',
+                'search_items' => 'Search Stations',
+                'not_found' => 'No stations found',
+                'not_found_in_trash' => 'No stations found in trash',
+            ),
+        'description' => 'Partner Stations',
+        'public' => true,
+        'taxonomies' => array(),
+        )
+    );
+};
+
+?>
