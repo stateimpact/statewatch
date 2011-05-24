b86b4f880266219bd5585f0bccebf86576b657fd
Chris Amico <eyeseast@gmail.com>
2011-05-23 17:14:49 -0700

    Stations now get metadata

--- includes/stations.php
+++ includes/stations.php
@@ -1,6 +1,5 @@
 <?php
 add_action( 'init', 'create_station_post_types' );
-
 function create_station_post_types() {
     register_post_type( 'partner_station', 
         array(
@@ -25,5 +24,49 @@ function create_station_post_types() {
         )
     );
 };
+
+add_action( 'add_meta_boxes', 'add_station_metabox');
+function add_station_metabox() {
+    add_meta_box( 'station_metadata', 'Station Metadata', 
+                  'station_metabox', 'partner_station', 'normal', 'high');
+};
+
+function station_metabox($post) {
+    $freq = get_post_meta( $post->ID, 'frequency', true );
+    $city = get_post_meta( $post->ID, 'city', true );
+    $support_url = get_post_meta( $post->ID, 'support_url', true); ?>
+    <table class="form-tabel">
+        <tr>
+            <th><label for="frequency">Frequency: </label></th>
+            <td>
+                <input type="text" name="frequency" value="<?php echo esc_attr( $freq ); ?>"/>
+                <span class="description">For example, "88.5 FM"</span>
+            </td>
+        <tr>
+            <th><label for="city">City: </label></th>
+            <td>
+                <input type="text" name="city" value="<?php echo esc_attr( $city ); ?>"/>
+                <span class="description">For example, "Washington, DC"</span>
+            </td>
+        </tr>
+        <tr>
+            <th><label for="support_url">Support URL: </label></th>
+            <td>
+                <input type="text" name="support_url" value="<?php echo esc_attr( $support_url ); ?>"/>
+                <span class="description">Where can people give you money?</span>
+            </td>
+        </tr>
+    </table>
+<?php 
+}
+
+add_action( 'save_post', 'save_station_metadata' );
+function save_station_metadata($post_id) {
+    $fields = array('frequency', 'city', 'support_url');
+    foreach ($fields as $field) {
+        update_post_meta( $post_id, $field, 
+            strip_tags( $_POST[$field] ));
+    };
+}
 
 ?>
