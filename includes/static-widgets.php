b0b4fee15a994d4dc6630bc39293cfeacb3a4de1
Wesley Lindamod <wesley.lindamood@gmail.com>
2011-05-26 12:06:43 -0400

    adding structural markup and css for about widget, network news widget, and beyond statewatch widget

--- includes/static-widgets.php
+++ includes/static-widgets.php
@@ -8,26 +8,25 @@ add_action( 'widgets_init', 'argo_load_static_widgets' );
  * Register our widget.
  */
 function argo_load_static_widgets() {
-    register_widget( 'crowd_Widget' );
-    register_widget( 'discussions_Widget' );
-    register_widget( 'must_Widget' );
-    register_widget( 'flick_Widget' );
+    register_widget( 'iog_abt_Widget' );
+    register_widget( 'iog_network_news_Widget' );
+    register_widget( 'iog_links_Widget' );
 }
 
 /**
- * crowd Widget class.
+ * about Widget class.
  */
-class crowd_Widget extends WP_Widget {
+class iog_abt_Widget extends WP_Widget {
 
 	/**
 	 * Widget setup.
 	 */
-	function crowd_Widget() {
+	function iog_abt_Widget() {
 		/* Widget settings. */
-		$widget_ops = array( 'classname' => 'crowd', 'description' => __('Static placeholder', 'crowd') );
+		$widget_ops = array( 'classname' => 'iog_abt', 'description' => __('Static placeholder', 'iog_abt') );
 
 		/* Create the widget. */
-		$this->WP_Widget( 'crowd-widget', __('crowd Widget', 'crowd'), $widget_ops);
+		$this->WP_Widget( 'iog_abt', __('About Widget', 'about'), $widget_ops);
 	}
 
 	/**
@@ -39,22 +38,32 @@ class crowd_Widget extends WP_Widget {
 		/* Before widget (defined by themes). */
 		echo $before_widget; ?>
 
-<h3>CROWD PICKS</h3>
-<div class="current-call clearfix">
+<div class="sw-about"> 
+<h3>About StateWatch Florida</h3> 
+ 
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p> 
+ 
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p> 
+ 
+<ul class="sw-social clearfix"> 
+<li class="sw-twitter"><a href="#">Twitter</a></li> 
+<li class="sw-fb"><a href="#">Facebook</a></li> 
+<li class="sw-rss"><a href="#">RSS</a></li> 
+</ul> 
+ 
+<ul class="sw-info clearfix"> 
+<li><a href="#">Learn more about StateWatch</a></li> 
+<li><a href="#">Support StateWatch</a></li> 
+</ul> 
+ 
+<form role="search" method="get" action="http://statewatch.argoproject.org/florida"> 
+	<div><label class="visuallyhidden" for="s1">Search for:</label> 
+	<input type="text" placeholder="Search StateWatch Virgina" value="" name="s" class="sw-about-search" /> 
+	<input type="image" src="http://statewatch.argoproject.org/florida/wp-content/themes/argo-foundation/img/btn-go.png" alt="Submit" width="32" height="24" id="search-submit" /> 
+	</div> 
+</form> 
 
-<img src="<?php bloginfo('template_directory'); ?>/img/dev-images/hand.jpg" alt="140x140" width="140" height="140" />
-
-<h4><a href="#" title="note">Which organizations do you give to?</a></h4>
-<p>From what we know of our crowd, you're a pretty compassionate bunch. So who do you give your money to? <a href="#" title="vote now">VOTE NOW</a></p>
-
-</div>
-<!-- /.current-call -->
-<h5>PREVIOUS CROWD PICKS</h5>
-<ul>
-<li><a href="#" title="headline">Who's history's greatest global health visionary? </a></li>
-<li><a href="#" title="headline">Which medical innovations are you most excited for?</a></li>
-<li><a href="#" title="headline">Best global pandemic films?</a></li>
-</ul>
+</div><!-- .sw-about -->
 
 
 <?php		/* After widget (defined by themes). */
@@ -63,19 +72,19 @@ class crowd_Widget extends WP_Widget {
 
 }
 /**
- * discussions Widget class.
+ * network news Widget class.
  */
-class discussions_Widget extends WP_Widget {
+class iog_network_news_Widget extends WP_Widget {
 
 	/**
 	 * Widget setup.
 	 */
-	function discussions_Widget() {
+	function iog_network_news_Widget() {
 		/* Widget settings. */
-		$widget_ops = array( 'classname' => 'discussions', 'description' => __('Static placeholder', 'discussions') );
+		$widget_ops = array( 'classname' => 'iog_network_news', 'description' => __('Static placeholder', 'iog_network_news') );
 
 		/* Create the widget. */
-		$this->WP_Widget( 'discussions-widget', __('discussions Widget', 'discussions'), $widget_ops);
+		$this->WP_Widget( 'iog_network_news', __('Network News Widget', 'NN'), $widget_ops);
 	}
 
 	/**
@@ -87,36 +96,21 @@ class discussions_Widget extends WP_Widget {
 		/* Before widget (defined by themes). */
 		echo $before_widget; ?>
 
-<h3>MOST COMMENTED</h3>
-
-<ul>
-<li class="clearfix">
-<strong  class="comment-count level3">
-<span>1,161</span> comments
-</strong>
-<a href="#" title="headline">
-Partying for prevention
-</a>
-</li>
-
-<li class="clearfix">
-<strong  class="comment-count level2">
-<span>500</span> comments
-</strong>
-<a href="#" title="headline">
-The 5 biggest global health controversies
-</a>
-</li>
-
-<li class="clearfix">
-<strong  class="comment-count level1">
-<span>250</span> comments
-</strong>
-<a href="#" title="headline">
-Empowering women to stop rape in the Congo
-</a>
-</li>
-</ul>
+<div class="sw-network-news"> 
+<h3>StateWatch Network News</h3> 
+<p class="swnn-tagline">Issues that matter. Close to home.</p> 
+<h6>StateWatch Florida</h6> 
+<h4><a href="#">Lorem ipsum dolor sit amet</a></h4> 
+<img src="http://statewatch.argoproject.org/florida/wp-content/themes/statewatch/img/dev-img/60x60.png"> 
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p> 
+ 
+<h6>StateWatch Ohio</h6> 
+<h5><a href="#">Lorem ipsum dolor sit amet</a></h5> 
+ 
+<h6>StateWatch Idaho</h6> 
+<h5><a href="#">Lorem ipsum dolor sit amet</a></h5> 
+ 
+</div><!-- .sw-network-news -->
 
 <?php		/* After widget (defined by themes). */
 		echo $after_widget;
@@ -124,19 +118,19 @@ Empowering women to stop rape in the Congo
 
 }
 /**
- * must Widget class.
+ * Beyond Statewatch Widget class.
  */
-class must_Widget extends WP_Widget {
+class iog_links_Widget extends WP_Widget {
 
 	/**
 	 * Widget setup.
 	 */
-	function must_Widget() {
+	function iog_links_Widget() {
 		/* Widget settings. */
-		$widget_ops = array( 'classname' => 'must', 'description' => __('Static placeholder', 'must') );
+		$widget_ops = array( 'classname' => 'iog_links', 'description' => __('Static placeholder', 'iog_links') );
 
 		/* Create the widget. */
-		$this->WP_Widget( 'must-widget', __('must Widget', 'must'), $widget_ops);
+		$this->WP_Widget( 'iog_links', __('Beyond Widget', 'beyond'), $widget_ops);
 	}
 
 	/**
@@ -146,67 +140,41 @@ class must_Widget extends WP_Widget {
 		extract( $args );
 
 		/* Before widget (defined by themes). */
-		echo $before_widget;?>
+		echo $before_widget; ?>
 
-<h3>MUST READS</h3>
-<ul>
-<li><img src="<?php bloginfo('template_directory'); ?>/img/dev-images/must-reads.jpg" alt="60x60a" width="60" height="60" />
-<h4><a href="#" title="headline">The top 20 global health priorities</a></h4>
-<h6>Time</h6>
-<p>In one of the world's largest public-health collaborations, 155 experts from 50 countries have a plan to tackle the world's deadliest diseases.</p>
-</li>
+<div id="beyond-statewatch"> 
+<h3>Beyond StateWatch</h3> 
+<ul> 
+<li> 
+<h5><a href="#">A National Lab Develops Grid Controls to Handle Renewable Energy</a></h5> 
+<p><img src="favicon.png" alt="favicon" width="16" height="16">NYTimes.com via bookmark (4 hours ago)</p> 
+</li> 
+ 
+<li> 
+<h5><a href="#">A National Lab Develops Grid Controls to Handle Renewable Energy</a></h5> 
+<p><img src="favicon.png" alt="favicon" width="16" height="16">NYTimes.com via bookmark (4 hours ago)</p> 
+</li> 
+ 
+<li> 
+<h5><a href="#">A National Lab Develops Grid Controls to Handle Renewable Energy</a></h5> 
+<p><img src="favicon.png" alt="favicon" width="16" height="16">NYTimes.com via bookmark (4 hours ago)</p> 
+</li> 
+ 
+<li> 
+<h5><a href="#">A National Lab Develops Grid Controls to Handle Renewable Energy</a></h5> 
+<p><img src="favicon.png" alt="favicon" width="16" height="16">NYTimes.com via bookmark (4 hours ago)</p> 
+</li> 
+ 
+<li> 
+<h5><a href="#">A National Lab Develops Grid Controls to Handle Renewable Energy</a></h5> 
+<p><img src="favicon.png" alt="favicon" width="16" height="16">NYTimes.com via bookmark (4 hours ago)</p> 
+</li> 
+ 
+</ul> 
+</div><!-- #beyond-statewatch -->
 
-<li>
-<h5><a href="#" title="headline">The 5 biggest global health controversies</a></h5>
-<h6>Change.org</h6>
-</li>
 
-<li>
-<h5><a href="#" title="headline">The US Global Health Initiative: Key issues</a></h5>
-<h6>Kaiser Health News</h6>
-</li>
-</ul>
-
-<?php /* After widget (defined by themes). */
-		echo $after_widget;
-	}
-
-}
-/**
- * flick Widget class.
- */
-class flick_Widget extends WP_Widget {
-
-	/**
-	 * Widget setup.
-	 */
-	function flick_Widget() {
-		/* Widget settings. */
-		$widget_ops = array( 'classname' => 'flick', 'description' => __('Static placeholder', 'flick') );
-
-		/* Create the widget. */
-		$this->WP_Widget( 'flick-widget', __('flick Widget', 'flick'), $widget_ops);
-	}
-
-	/**
-	 * How to display the widget on the screen.
-	 */
-	function widget( $args, $instance ) {
-		extract( $args );
-
-		/* Before widget (defined by themes). */
-		echo $before_widget;?>
-
-<h3>VITAL SIGNS PHOTOS</h3>
-<p><strong>ADD YOURS.</strong> Tag your photos #tagname on Flickr.</p>
-
-<div class="flickr-badge clearfix">
-<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=8&display=latest&size=s&layout=h&source=user_set&user=26008544%40N00&set=72157594235451142&context=in%2Fset-72157594235451142%2F"></script>
-</div>
-<!-- /Flickr Badge -->
-<p><a href="#" title="view all">VIEW ALL PHOTOS FOR THIS TOPIC</a></p>
-
-<?php /* After widget (defined by themes). */
+<?php		/* After widget (defined by themes). */
 		echo $after_widget;
 	}
 
