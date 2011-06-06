a18479e40f11a6306d8ed5775fea096cb2b9636a
Chris Amico <eyeseast@gmail.com>
2011-06-06 18:02:42 -0400

    Links working front and back. Still need some decision logic.

--- collection-description.php
+++ collection-description.php
@@ -1,23 +1,31 @@
+<?php
+$cat = $wp_query->get_queried_object();
+$topic = argo_get_topic_for( $cat );
+$links = sw_get_topic_featured_links( $topic );
+?>
 <div id="coll-intro" class="grid_12 coll-dl"> 
-<h2><?php single_tag_title(); ?></h2> 
+<h2><?php echo $topic->post_title; ?></h2>
 
 <div class="coll-desc clearfix"> 
 <h6>Background</h6>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p> 
-
-<p>Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed.</p>
+    <?php echo $topic->post_content; ?>    
 </div>
 
 <div class="coll-links">
 <h6>Key Links</h6>
 <ul>
+    <?php foreach( $links as $link ): ?>
+        <?php if ( $link['url'] ): ?>
+            <li><a href="<?php echo $link['url']; ?>"><?php echo $link['title']; ?></a> <span class="key-source">(<?php echo $link['source']; ?>)</span></li>
+        <?php endif; ?>
+    <?php endforeach; ?>
+    <!--
 <li><a href="#">Fracking with our food: how gas drilling affects farming</a> <span class="key-source">(Miami Herald)</span></li>
 <li><a href="#">Susquehanna River named most endangered in US due to fracking</a> <span class="key-source">(The Tennessean)</span></li>
 <li><a href="#">Fracking As A Fractured Relationship With Ourselves</a> <span class="key-source">(Treehugger)</span></li>
 <li><a href="#">Safety first on fracking</a> <span class="key-source">(Philadelphia Inquirer)</span></li>
 <li><a href="#">Dot Earth: New Tool, and Tune, for Tracking Fracking</a> <span class="key-source">(New York Times (blog))</span></li>
+-->
 </ul>
 </div>
 </div> <!-- /#coll-intro .grid_12 --> 
--- includes/topics.php
+++ includes/topics.php
@@ -9,4 +9,67 @@ function sw_topic_setup() {
         remove_action( 'edited_term', 'argo_create_topic_page', 10, 3 );
     }
 }
+
+add_action( 'add_meta_boxes', 'sw_topic_links_metabox' );
+function sw_topic_links_metabox() {
+    add_meta_box( 'featured-links', 'Featured Links', 'sw_featured_links',
+                  'topic', 'normal', 'high');
+}
+
+function sw_featured_links($post) { ?>
+    <table class="form-table">
+        <tr>
+            <td></td>
+            <th>Link Title</th>
+            <th>URL</th>
+            <th>Source</th>
+        </tr>
+        
+        <?php // this feels dirty ?>
+        <?php foreach( range(0, 4) as $i ): ?>
+        <tr>
+            <td><?php echo $i + 1; ?></td>
+            <?php $fields = array( 'title', 'url', 'source' ); ?>
+            <?php foreach( $fields as $field ): ?>
+            <td>
+                <?php $name = "link_" . $i . "_" . $field; ?>
+                <?php $value = get_post_meta( $post->ID, $name, true ); ?>
+                <input type="text" id="<?php echo $name; ?>" 
+                       name="<?php echo $name; ?>" 
+                       value="<?php echo $value; ?>" />
+            </td>
+            <?php endforeach; ?>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <?php
+}
+
+add_action( 'save_post', 'sw_update_topic_links' );
+function sw_update_topic_links($post_id) {
+    $fields = array( 'title', 'url', 'source' );
+    foreach( range(0, 4) as $i ) {
+        foreach( $fields as $field ) {
+            $name = "link_" . $i . "_" . $field;
+            $value = $_POST[$name];
+            update_post_meta( $post_id, $name, $value );
+        }
+    }
+}
+
+function sw_get_topic_featured_links($post) {
+    $results = array();
+    $fields = array( 'title', 'url', 'source' );
+    foreach( range(0, 4) as $i ) {
+        $link = array();
+        foreach( $fields as $field ) {
+            $name = "link_" . $i . "_" . $field;
+            $link[$field] = get_post_meta( $post->ID, $name, true );
+        }
+        $results[$i] = $link;
+    }
+    
+    return $results;
+}
+
 ?>
