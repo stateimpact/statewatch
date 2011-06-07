b265d3051b62d608f4c1ca01c6bb2e2bdd6bb4ea
Chris Amico <eyeseast@gmail.com>
2011-06-07 10:48:13 -0400

    Presentation logic on collection header

--- collection-description.php
+++ collection-description.php
@@ -2,30 +2,26 @@
 $cat = $wp_query->get_queried_object();
 $topic = argo_get_topic_for( $cat );
 $links = sw_get_topic_featured_links( $topic );
+$first_link = $links[0];
 ?>
 <div id="coll-intro" class="grid_12 coll-dl"> 
 <h2><?php echo $topic->post_title; ?></h2>
-
+<?php if ( $topic->post_content ): ?>
 <div class="coll-desc clearfix"> 
 <h6>Background</h6>
     <?php echo $topic->post_content; ?>    
 </div>
-
+<?php endif; ?>
+<?php if ( $first_link['url'] ) { ?>
 <div class="coll-links">
-<h6>Key Links</h6>
-<ul>
+    <h6>Key Links</h6>
+    <ul>
     <?php foreach( $links as $link ): ?>
         <?php if ( $link['url'] ): ?>
             <li><a href="<?php echo $link['url']; ?>"><?php echo $link['title']; ?></a> <span class="key-source">(<?php echo $link['source']; ?>)</span></li>
         <?php endif; ?>
     <?php endforeach; ?>
-    <!--
-<li><a href="#">Fracking with our food: how gas drilling affects farming</a> <span class="key-source">(Miami Herald)</span></li>
-<li><a href="#">Susquehanna River named most endangered in US due to fracking</a> <span class="key-source">(The Tennessean)</span></li>
-<li><a href="#">Fracking As A Fractured Relationship With Ourselves</a> <span class="key-source">(Treehugger)</span></li>
-<li><a href="#">Safety first on fracking</a> <span class="key-source">(Philadelphia Inquirer)</span></li>
-<li><a href="#">Dot Earth: New Tool, and Tune, for Tracking Fracking</a> <span class="key-source">(New York Times (blog))</span></li>
--->
-</ul>
+    </ul>
 </div>
+<?php }; ?>
 </div> <!-- /#coll-intro .grid_12 --> 
