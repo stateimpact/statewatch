7c3a368d9c27d4b41cbb087a434f3b302420b497
Danny DeBelius <danny.debelius@gmail.com>
2011-06-30 13:25:08 -0400

    Post page headline receives H1 tag per Javaun's SEO site review

--- single.php
+++ single.php
@@ -11,7 +11,7 @@
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
     <div id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 <header>
-<h2 class="entry-title"><?php the_title(); ?></h2>
+<h1 class="entry-title"><?php the_title(); ?></h1>
 
 	<div class="post-metadata clearfix">
 		<div class="grid_3 alpha">
