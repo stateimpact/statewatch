429a0758530cae1ff451659922c91a8c5683b06d
Danny DeBelius <danny.debelius@gmail.com>
2011-07-01 15:01:03 -0400

    Add 404

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Topic index
+*/
+?>
+
+<?php get_header(); ?>
+
+<article class="grid_8">
+<div id="content">
+<h1 class="entry-title">404 - Not Found</h1>
+<p>Sorry. We can't find the page you were looking for. Please try searching for related posts:</p>
+<div class="clearfix"></div>
+<?php get_search_form(); ?>
+</div>
+</article>
+<!--/.grid_8-->
+
+<aside id="sidebar" class="grid_4">
+<?php get_sidebar(); ?>
+</aside>
+<!-- /.grid_4 -->
+<?php get_footer(); ?>
---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" class="searchform" action="<?php navis_the_searchform_url(); ?>">
+	<div><label class="visuallyhidden" for="s2">Search for:</label>
+	<input type="text" placeholder="Search This Site" value="" name="s" id="s2" />
+	<input type="button" alt="Submit" width="32" height="24" class="search-submit" />
+	</div>
+</form>
