a3b7af2c86a83549af5612197e2db6a6991040fa
Danny DeBelius <danny.debelius@gmail.com>
2011-07-07 10:27:33 -0400

    New style Typekit font-family declarations

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 	?>
 	</title>
 	
-	<script type="text/javascript" src="http://use.typekit.com/xah5jnf.js"></script>
+	<script type="text/javascript" src="http://use.typekit.com/yey2zgp.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	
 	<link rel="icon" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon.png"/>
