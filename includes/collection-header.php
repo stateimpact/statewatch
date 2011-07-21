f8b110f77f8a17f697954b83c905d07425aa7a6c
Danny DeBelius <danny.debelius@gmail.com>
2011-07-21 11:57:32 -0400

    Tag buildout Twitter/FB sharing

--- includes/collection-header.php
+++ includes/collection-header.php
@@ -12,10 +12,10 @@ $first_link = $links[0];
 	    <h2><?php echo $topic->post_title; ?></h2>
 		<ul class="meta-gestures">
 		    <li class="twitter"> 
-		        <a href="<?php echo esc_url( 'http://twitter.com/share?url=' . $topic->guid . '&text=' ) . argo_get_twitter_title(); ?>" class="twitter-share-button" data-count="horizontal">Tweet</a>
+		        <a href="<?php echo esc_url( 'http://twitter.com/share?url=' . $topic->guid . '&text=' ) . rawurlencode( $topic->post_title ); ?>" class="twitter-share-button" data-count="horizontal">Tweet</a>
 		    </li>
 		    <li class="fb">
-		        <a name="fb_share" share_url="<?php echo esc_url( $topic->guid ); ?>" type="button_count" href="<?php echo esc_url( 'http://www.facebook.com/sharer.php?u=' . $topic->guid . '&t=' ) . get_the_title();  ?>">Share</a>
+		        <a name="fb_share" share_url="<?php echo esc_url( $topic->guid ); ?>" type="button_count" href="<?php echo esc_url( 'http://www.facebook.com/sharer.php?u=' . $topic->guid . '&t=' ) . rawurlencode( $topic->post_title );  ?>">Share</a>
 		    </li>
 		</ul>	
 	<?php else: ?>
