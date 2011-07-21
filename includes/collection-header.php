e2fcf55d44282fcc82b57b1df28061083f2372ac
Danny DeBelius <danny.debelius@gmail.com>
2011-07-21 10:44:14 -0400

    Fix tag buildout sharing

--- includes/collection-header.php
+++ includes/collection-header.php
@@ -12,10 +12,10 @@ $first_link = $links[0];
 	    <h2><?php echo $topic->post_title; ?></h2>
 		<ul class="meta-gestures">
 		    <li class="twitter"> 
-		        <a href="<?php echo esc_url( 'http://twitter.com/share?url=' . get_permalink() . '&text=' ) . argo_get_twitter_title(); ?>" class="twitter-share-button" data-count="horizontal">Tweet</a>
+		        <a href="<?php echo esc_url( 'http://twitter.com/share?url=' . $topic->guid . '&text=' ) . argo_get_twitter_title(); ?>" class="twitter-share-button" data-count="horizontal">Tweet</a>
 		    </li>
 		    <li class="fb">
-		        <a name="fb_share" share_url="<?php the_permalink(); ?>" type="button_count" href="<?php echo esc_url( 'http://www.facebook.com/sharer.php?u=' . get_permalink() . '&t=' ) . get_the_title();  ?>">Share</a>
+		        <a name="fb_share" share_url="<?php echo esc_url( $topic->guid ); ?>" type="button_count" href="<?php echo esc_url( 'http://www.facebook.com/sharer.php?u=' . $topic->guid . '&t=' ) . get_the_title();  ?>">Share</a>
 		    </li>
 		</ul>	
 	<?php else: ?>
