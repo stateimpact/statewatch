37a247974adfd55484bcc3b7a45c1c63021bafe2
Danny DeBelius <danny.debelius@gmail.com>
2011-07-22 15:38:29 -0400

    Styling for recent entries sidebar widget

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -1,6 +1,13 @@
 <?php
 
-add_action( 'init', 'sw_register_sidebars' );
+function remove_some_widgets(){
+	// Unregister foundation post sidebar to fix widget title bar markup
+	unregister_sidebar( 'post' );
+}
+add_action( 'init', 'remove_some_widgets', 11 );
+
+
+
 function sw_register_sidebars() {
     
     // sidebar-about.php
@@ -9,6 +16,17 @@ function sw_register_sidebars() {
         'name'        => 'About Page Sidebar',
         'description' => 'A special sidebar for the about page'
     ) );
+
+	// POST PAGE SIDEBAR
+    register_sidebar( array(
+        'id'           => 'post',
+        'name'         => 'Post Page Sidebar',
+        'description'  => 'Sidebar in right rail of post pages.',
+		'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
 }
+add_action( 'init', 'sw_register_sidebars', 12 );
+
 
 ?>
