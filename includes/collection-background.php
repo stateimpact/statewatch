3fea0ca312c146f2de77a762c4a39ce1a928d976
Danny DeBelius <danny.debelius@gmail.com>
2011-07-28 11:20:30 -0400

    Collection backgrounds should get all .post styles. Added table style, specifically.

--- includes/collection-background.php
+++ includes/collection-background.php
@@ -3,7 +3,7 @@ $cat = $wp_query->get_queried_object();
 $topic = argo_get_topic_for( $cat );
 ?>
 <?php if ( $topic->post_content ): ?>
-	<div class="coll-desc clearfix"> 
+	<div class="coll-desc post clearfix"> 
 		<h6>Background</h6>
 	    <?php echo apply_filters( 'the_content', $topic->post_content ); ?>
 	</div>
