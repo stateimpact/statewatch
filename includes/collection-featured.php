0826ae365ca0fce683bf42d7f273d1386bb62f57
Danny DeBelius <danny.debelius@gmail.com>
2011-12-14 14:27:54 -0500

    Fix date, thumb size in featured stories, add bottom margin

--- includes/collection-featured.php
+++ includes/collection-featured.php
@@ -9,9 +9,9 @@ $featured = sw_get_topic_featured_posts( $topic->ID );
     	<h2 class="section-hed">Key Stories</h2>
         <?php foreach($featured as $post): ?>
     	<div class="story">
-    		<h4 class="pub-date"><?php the_date('F j, Y'); ?></h4>
+    		<h4 class="pub-date"><?php the_time('F j, Y'); ?></h4>
     		<?php if (has_post_thumbnail()): ?>
-                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail', array('class'=>'alignleft')); ?></a>
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(100,100), array('class'=>'alignright')); ?></a>
     		<?php endif; ?>
     		<h3 class="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
     	</div>
