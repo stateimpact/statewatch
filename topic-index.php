3e9ccf84a46bb0ca1c0a56280972423bc7917779
Danny DeBelius <danny.debelius@gmail.com>
2012-05-23 14:13:38 -0400

    Searchable topic index

--- topic-index.php
+++ topic-index.php
@@ -26,7 +26,7 @@ Template Name: Topic index
 <?php endwhile; ?>
 
 </div><!-- #content -->
-
+<!-- 
 <nav class="alpha-nav clearfix">
     <ul>
         <li><a href="#a">A</a></li>
@@ -57,8 +57,107 @@ Template Name: Topic index
         <li><a href="#z">Z</a></li>
     </ul>
 </nav>
+-->
 <!-- /.alpha-nav -->
 
+<script type="text/javascript" charset="utf-8">
+String.prototype.score = function(abbreviation,offset) {
+  offset = offset || 0 // TODO: I think this is unused... remove
+ 
+  if(abbreviation.length == 0) return 0.9
+  if(abbreviation.length > this.length) return 0.0
+
+  for (var i = abbreviation.length; i > 0; i--) {
+    var sub_abbreviation = abbreviation.substring(0,i)
+    var index = this.indexOf(sub_abbreviation)
+
+
+    if(index < 0) continue;
+    if(index + abbreviation.length > this.length + offset) continue;
+
+    var next_string       = this.substring(index+sub_abbreviation.length)
+    var next_abbreviation = null
+
+    if(i >= abbreviation.length)
+      next_abbreviation = ''
+    else
+      next_abbreviation = abbreviation.substring(i)
+ 
+    var remaining_score   = next_string.score(next_abbreviation,offset+index)
+ 
+    if (remaining_score > 0) {
+      var score = this.length-next_string.length;
+
+      if(index != 0) {
+        var j = 0;
+
+        var c = this.charCodeAt(index-1)
+        if(c==32 || c == 9) {
+          for(var j=(index-2); j >= 0; j--) {
+            c = this.charCodeAt(j)
+            score -= ((c == 32 || c == 9) ? 1 : 0.15)
+          }
+        } else {
+          score -= index
+        }
+      }
+   
+      score += remaining_score * next_string.length
+      score /= this.length;
+      return score
+    }
+  }
+  return 0.0
+}
+jQuery.fn.liveUpdate = function(list){
+  list = jQuery(list);
+  if ( list.length ) {
+    var rows = list.children('li'),
+      cache = rows.map(function(){
+        return this.firstChild.innerHTML.toLowerCase();
+      });
+      
+    this
+      .keyup(filter).keyup()
+      .parents('form').submit(function(){
+        return false;
+      });
+  }
+    
+  return this;
+    
+  function filter(){
+    var term = jQuery.trim( jQuery(this).val().toLowerCase() ), scores = [];
+    
+    if ( !term ) {
+      rows.show();
+    } else {
+      rows.hide();
+
+      cache.each(function(i){
+        var score = this.score(term);
+        if (score > 0) { scores.push([score, i]); }
+      });
+
+      jQuery.each(scores.sort(function(a, b){return b[0] - a[0];}), function(){
+        jQuery(rows[ this[1] ]).show();
+      });
+    }
+  }
+};
+
+
+
+    jQuery(document).ready(function() {
+        jQuery('#term').liveUpdate('#tags').focus();
+    });
+</script>
+
+    <form method="get">
+        <div>
+            <input type="text" value="" name="q" id="term" placeholder="Search all topics" />
+        </div> 
+    </form>
 <?php
     $query_string = '
 		SELECT *,name FROM '.$wpdb->prefix.'term_taxonomy
@@ -70,30 +169,14 @@ Template Name: Topic index
 	$post_tags = $wpdb->get_results($query_string);
 	?>
 	
-	<div class="number abc_tags">
-		<ul>
+	<div class="">
+		<ul id="tags" class="abc_tags">
 	<?php
 	foreach($post_tags as $key => $tag) {
-		$newletter = substr($tag->slug, 0, 1);
-		if($newletter !== $letter/* && $key != 0*/) { ?>
-		</ul>
-		<p><a href="#content">Back to top  &uarr;</a></p>
-	</div>
-	
-	<div class="<?php echo strtolower($newletter); ?> abc_tags clearfix">
-	
-	<div class="tag-letter">
-	<h3 id="<?php echo strtolower($newletter); ?>"><?php echo strtolower($newletter); ?></h3>
-	
-	</div>
-	
-		<ul>
-	<?php	} $letter = substr($tag->slug, 0, 1); ?>
-			<li><a href="<?php echo get_tag_link($tag->term_id); ?>" title="<?php echo sprintf( __( "View all posts in %s" ), $tag->name ); ?>"><?php echo $tag->name.' <span class="count">'.$tag->count.'</span>';?></a></li>
+    ?>
+			<li><a href="<?php echo get_tag_link($tag->term_id); ?>" title="<?php echo sprintf( __( "View all posts in %s" ), $tag->name ); ?>"><?php echo $tag->name.' </a><span class="count">'.$tag->count.'</span>';?></li>
 			
-		<?php
-	}
-		?>
+	<?php }	?>
 		
 		</ul>
 		
