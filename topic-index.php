1139269883e0f241e28337079b8ab223252c7354
Danny DeBelius <danny.debelius@gmail.com>
2012-05-24 11:17:30 -0400

    Move JS out of template

--- topic-index.php
+++ topic-index.php
@@ -4,6 +4,14 @@ Template Name: Topic index
 */
 ?>
 
+<?php
+function sw_topic_filtering() {
+        $js = get_bloginfo('stylesheet_directory') . "/js/topic-filtering.js";
+        wp_enqueue_script('topicfilter', $js);
+}    
+add_action('wp_enqueue_scripts', 'sw_topic_filtering');
+?>
+
 <?php get_header(); ?>
 
 <article class="grid_8">
@@ -26,133 +34,6 @@ Template Name: Topic index
 <?php endwhile; ?>
 
 </div><!-- #content -->
-<!-- 
-<nav class="alpha-nav clearfix">
-    <ul>
-        <li><a href="#a">A</a></li>
-        <li><a href="#b">B</a></li>
-        <li><a href="#c">C</a></li>
-        <li><a href="#d">D</a></li>
-        <li><a href="#e">E</a></li>
-        <li><a href="#f">F</a></li>
-        <li><a href="#g">G</a></li>
-        <li><a href="#h">H</a></li>
-        <li><a href="#i">I</a></li>
-        <li><a href="#j">J</a></li>
-        <li><a href="#k">K</a></li>
-        <li><a href="#l">L</a></li>
-        <li><a href="#m">M</a></li>
-        <li><a href="#n">N</a></li>
-        <li><a href="#o">O</a></li>
-        <li><a href="#p">P</a></li>
-        <li><a href="#q">Q</a></li>
-        <li><a href="#r">R</a></li>
-        <li><a href="#s">S</a></li>
-        <li><a href="#t">T</a></li>
-        <li><a href="#u">U</a></li>
-        <li><a href="#v">V</a></li>
-        <li><a href="#w">W</a></li>
-        <li><a href="#x">X</a></li>
-        <li><a href="#y">Y</a></li>
-        <li><a href="#z">Z</a></li>
-    </ul>
-</nav>
--->
-<!-- /.alpha-nav -->
-
-<script type="text/javascript" charset="utf-8">
-String.prototype.score = function(abbreviation,offset) {
-  offset = offset || 0 // TODO: I think this is unused... remove
- 
-  if(abbreviation.length == 0) return 0.9
-  if(abbreviation.length > this.length) return 0.0
-
-  for (var i = abbreviation.length; i > 0; i--) {
-    var sub_abbreviation = abbreviation.substring(0,i)
-    var index = this.indexOf(sub_abbreviation)
-
-
-    if(index < 0) continue;
-    if(index + abbreviation.length > this.length + offset) continue;
-
-    var next_string       = this.substring(index+sub_abbreviation.length)
-    var next_abbreviation = null
-
-    if(i >= abbreviation.length)
-      next_abbreviation = ''
-    else
-      next_abbreviation = abbreviation.substring(i)
- 
-    var remaining_score   = next_string.score(next_abbreviation,offset+index)
- 
-    if (remaining_score > 0) {
-      var score = this.length-next_string.length;
-
-      if(index != 0) {
-        var j = 0;
-
-        var c = this.charCodeAt(index-1)
-        if(c==32 || c == 9) {
-          for(var j=(index-2); j >= 0; j--) {
-            c = this.charCodeAt(j)
-            score -= ((c == 32 || c == 9) ? 1 : 0.15)
-          }
-        } else {
-          score -= index
-        }
-      }
-   
-      score += remaining_score * next_string.length
-      score /= this.length;
-      return score
-    }
-  }
-  return 0.0
-}
-jQuery.fn.liveUpdate = function(list){
-  list = jQuery(list);
-  if ( list.length ) {
-    var rows = list.children('tr'),
-        cache = rows.map(function(){
-
-          return jQuery(this).first('a').text().toLowerCase();
-        });
-      
-    this
-      .keyup(filter).keyup()
-      .parents('form').submit(function(){
-        return false;
-      });
-  }
-    
-  return this;
-    
-  function filter(){
-    var term = jQuery.trim( jQuery(this).val().toLowerCase() ), scores = [];
-    
-    if ( !term ) {
-      rows.show();
-    } else {
-      rows.hide();
-
-      cache.each(function(i){
-        var score = this.score(term);
-        if (score > 0) { scores.push([score, i]); }
-      });
-
-      jQuery.each(scores.sort(function(a, b){return b[0] - a[0];}), function(){
-        jQuery(rows[ this[1] ]).show();
-      });
-    }
-  }
-};
-
-
-
-    jQuery(document).ready(function() {
-        jQuery('#term').liveUpdate('#tags tbody').focus();
-    });
-</script>
 
     <form method="get" autocomplete="off">
         <div>
@@ -200,4 +81,5 @@ jQuery.fn.liveUpdate = function(list){
 <?php get_sidebar(); ?>
 </aside>
 <!-- /.grid_4 -->
+
 <?php get_footer(); ?>
