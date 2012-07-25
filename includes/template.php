a7e63574a40ce9607eb4d1a18a107c16665a7c6e
Danny DeBelius <danny.debelius@gmail.com>
2012-07-25 12:31:50 -0400

    No mobile styles if fusion tables map (yet)

--- includes/template.php
+++ includes/template.php
@@ -132,7 +132,8 @@ function sw_mobile_style() {
     if (is_single()) {
         global $post;
         if (sw_has_wide_assets($post->ID)
-            || get_post_meta($post->ID, 'custom_post_template', true) == SINGLE_FULL_WIDTH) {
+            || get_post_meta($post->ID, 'custom_post_template', true) == SINGLE_FULL_WIDTH
+            || get_post_type($post->ID) == 'fusiontablesmap') {
             // error_log("Wide post. Not loading adaptive styles.");
             return;
         }
