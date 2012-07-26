55985887d8c4bb0c255bda29a85a22633758a109
Danny DeBelius <danny.debelius@gmail.com>
2012-07-26 10:23:34 -0400

    Don't use enqeue scripts for twitter widget

--- functions.php
+++ functions.php
@@ -175,7 +175,7 @@ add_action('wp_enqueue_scripts', 'sw_social');
 function sw_social() {
     $twitter = '//platform.twitter.com/widgets.js';
     $fb = 'http://static.ak.fbcdn.net/connect.php/js/FB.Share';
-    wp_enqueue_script('twitter-widgets', $twitter);
+    //wp_enqueue_script('twitter-widgets', $twitter);
     //wp_enqueue_script('facebook', $fb);
 }
 
